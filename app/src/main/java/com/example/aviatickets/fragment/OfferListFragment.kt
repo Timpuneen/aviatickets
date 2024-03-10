e8a81d7b20085785c056eba0a873423009dfe4b4
Timur Baltabayev <timpuh_work@mail.ru>
2024-03-11 00:34:25 +0600

    done

---
+++ app/src/main/java/com/example/aviatickets/fragment/OfferListFragment.kt
@@ -0,0 +1,79 @@
+package com.example.aviatickets.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.aviatickets.R
+import com.example.aviatickets.adapter.OfferListAdapter
+import com.example.aviatickets.databinding.FragmentOfferListBinding
+import com.example.aviatickets.model.entity.Offer
+import com.example.aviatickets.model.network.ApiClient
+import com.example.aviatickets.model.service.FakeService
+import retrofit2.Call
+import retrofit2.Response
+
+
+class OfferListFragment : Fragment() {
+    private var flightOffers: List<Offer> = emptyList()
+
+    companion object {
+        fun newInstance() = OfferListFragment()
+    }
+
+    private var _binding: FragmentOfferListBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private val adapter: OfferListAdapter by lazy {
+        OfferListAdapter()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOfferListBinding.inflate(layoutInflater, container, false)
+        return _binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupUI()
+        ApiClient.apiService.getOffers().enqueue(object : retrofit2.Callback<List<Offer>> {
+            override fun onResponse(call: Call<List<Offer>>, response: Response<List<Offer>>) {
+                if (response.isSuccessful) {
+                    flightOffers = response.body() ?: emptyList()
+                    adapter.submitList(flightOffers)
+
+                } else {
+                    Toast.makeText(requireContext(), "Something is wrong!", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+            override fun onFailure(call: Call<List<Offer>>, t: Throwable) {
+            }
+        })
+    }
+
+    private fun setupUI() {
+        with(binding) {
+            offerList.adapter = adapter
+
+            sortRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+                when (checkedId) {
+                    R.id.sort_by_price -> {
+
+                    }
+                    R.id.sort_by_duration -> {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/aviatickets/model/network/ApiClient.kt
@@ -0,0 +1,16 @@
+package com.example.aviatickets.model.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://my-json-server.typicode.com/estharossa/fake-api-demo/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val apiService: Service by lazy {
+        retrofit.create(Service::class.java)
+    }
+}
